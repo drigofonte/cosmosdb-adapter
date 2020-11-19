2ef4b94e4e096db9f6ef3899441baa4b767bcc5d
Rodrigo Fontenele Carvalho <1543402+drigofonte@users.noreply.github.com>
2020-11-19 16:52:42 +0100

    Added assignFunc to the cosmos document class constructor

--- cosmos-document.js
+++ cosmos-document.js
@@ -1,77 +1,90 @@
 const { CosmosDbAdapter } = require('./cosmos-adapter');
 
 class CosmosDocument {
-    constructor(id, partitionId, container, url, key, db) {
-        this.id = id;
-        this.partitionId = partitionId;
-        this.container = container;
-        this.url = url;
-        this.key = key;
-        this.db = db;
+  /**
+   * @param {String} id 
+   * @param {String} partitionId 
+   * @param {String} container 
+   * @param {String} url 
+   * @param {String} key 
+   * @param {String} db 
+   * @param {Function} assignFunc 
+   */
+  constructor(id, partitionId, container, url, key, db, assignFunc) {
+    this.id = id;
+    this.partitionId = partitionId;
+    this.container = container;
+    this.url = url;
+    this.key = key;
+    this.db = db;
+    this.assignFunc = assignFunc;
+  }
+
+  /**
+   * To be overriden by extensions of this class.
+   * 
+   * @returns {Boolean} true if the write should go ahead, false otherwise
+   */
+  onBeforeWrite() { return true; }
+
+  async write() {
+    if (this.onBeforeWrite()) {
+      const cosmosdb = new CosmosDbAdapter(this.url, this.key);
+      const obj = { ...this };
+      delete obj.partitionId;
+      delete obj.container;
+      delete obj.key;
+      delete obj.url;
+      delete obj.db;
+
+      if (this.id === undefined) {
+          await cosmosdb.write(this.db, this.container, obj);
+      } else {
+          await cosmosdb.replace(this.db, this.container, this.id, this.partitionId, obj);
+      }
     }
+    return this;
+  }
 
-    /**
-     * To be overriden by extensions of this class.
-     * 
-     * @returns {Boolean} true if the write should go ahead, false otherwise
-     */
-    onBeforeWrite() { return true; }
+  /**
+   * @param {String} query 
+   */
+  async getSingle(query) {
+    return await CosmosDocument.getSingle(query, this.container, this.assignFunc, this.url, this.key, this.db);
+  }
 
-    /**
-     * To be overriden by extensions of this class.
-     * 
-     * @returns {Function}
-     */
-    assignFunc() { return () => {} }
+  /**
+   * @param {String} query 
+   */
+  async getAll(query) {
+    return await CosmosDocument.getAll(query, this.container, this.assignFunc, this.url, this.key, this.db);
+  }
 
-    async write() {
-        if (this.onBeforeWrite()) {
-            const cosmosdb = new CosmosDbAdapter(this.url, this.key);
-            const obj = { ...this };
-            delete obj.partitionId;
-            delete obj.container;
-            delete obj.key;
-            delete obj.url;
-            delete obj.db;
+  toJSON() {
+    const obj = { ...this };
+    delete obj.partitionId;
+    delete obj.container;
+    delete obj.key;
+    delete obj.url;
+    delete obj.db;
+    return obj;
+  }
 
-            if (this.id === undefined) {
-                await cosmosdb.write(this.db, this.container, obj);
-            } else {
-                await cosmosdb.replace(this.db, this.container, this.id, this.partitionId, obj);
-            }
-        }
-        return this;
+  static async getSingle(query, container, assignFunc, url, key, db) {
+    const cosmosdb = new CosmosDbAdapter(url, key);
+    const response = await cosmosdb.execute(db, container, query);
+    let obj;
+    if (response.resources.length > 0) {
+        obj = assignFunc(response.resources[0]);
     }
+    return obj;
+  }
 
-    async getSingle(query) {
-        return await CosmosDocument.getSingle(query, this.container, this.assignFunc, this.url, this.key, this.db);
-    }
-
-    toJSON() {
-        const obj = { ...this };
-        delete obj.partitionId;
-        delete obj.container;
-        delete obj.key;
-        delete obj.url;
-        delete obj.db;
-        return obj;
-    }
-
-    static async getSingle(query, container, assignFunc, url, key, db) {
-        const cosmosdb = new CosmosDbAdapter(url, key);
-        const response = await cosmosdb.execute(db, container, query);
-        let obj;
-        if (response.resources.length > 0) {
-            obj = assignFunc(response.resources[0]);
-        }
-        return obj;
-    }
-
-    static async getAll(query, container, assignFunc, url, key, db) {
-      const cosmosdb = new CosmosDbAdapter(url, key);
-      const response = await cosmosdb.execute(db, container, query);
-      return response.resources.map(r => assignFunc(r));
-    }
+  static async getAll(query, container, assignFunc, url, key, db) {
+    const cosmosdb = new CosmosDbAdapter(url, key);
+    const response = await cosmosdb.execute(db, container, query);
+    return response.resources.map(r => assignFunc(r));
+  }
 }
 
 module.exports.CosmosDocument = CosmosDocument;
